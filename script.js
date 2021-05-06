a5782fbb579c3a478fd371d7e7aca15331653d48
Filimone Leaaetoa <filimone.leaaetoa@gmail.com>
2021-05-06 17:11:59 +1000

    centered text in hero banner

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const menuToggle = document.querySelector(".menu-btn");
 const showcase = document.querySelector(".showcase");
 const meneSelections = document.querySelectorAll(".menu-selections");
+const aboutMeText = document.querySelector(".text-about-me");
+const audio = document.querySelector("embed");
+
+audio.volume = 0.2;
 
 menuToggle.addEventListener("click", (e) => {
   menuToggle.classList.toggle("active");
