d8d3c7ba420d072649792667181a5eae4cc6380f
Filimone Leaaetoa <filimone.leaaetoa@gmail.com>
2021-05-06 22:42:05 +1000

    updated about me - merged tech & projects together

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 const menuToggle = document.querySelector(".menu-btn");
 const showcase = document.querySelector(".showcase");
-const meneSelections = document.querySelectorAll(".menu-selections");
+const menuSelections = document.querySelectorAll(".menu-selections");
 const aboutMeText = document.querySelector(".text-about-me");
-
 
 menuToggle.addEventListener("click", (e) => {
   menuToggle.classList.toggle("active");
   showcase.classList.toggle("active");
 });
 
-meneSelections.forEach((selection) => {
+menuSelections.forEach((selection) => {
   selection.addEventListener("click", (e) => {
     menuToggle.classList.remove("active");
     showcase.classList.remove("active");
+
+    if (aboutMeText.classList.value.includes("active")) {
+      aboutMeText.classList.remove("active");
+    }
+    aboutMeText.classList.toggle("active");
   });
 });
